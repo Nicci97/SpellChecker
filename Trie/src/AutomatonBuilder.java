07c58f7572c2e7a3d2c4f49236870d7dd96e26cf
Nicole <19478275@sun.ac.za>
2019-04-14 19:29:48 +0200

    spell checker added to trie

--- Trie/src/AutomatonBuilder.java
+++ Trie/src/AutomatonBuilder.java
@@ -5,6 +5,7 @@
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class AutomatonBuilder {
@@ -30,7 +31,7 @@ public class AutomatonBuilder {
         System.out.println(str);
     }
 
-    public static void doDFS() {
+    public static ArrayList<String> doDFS() {
         ArrayList<String> results = new ArrayList<>();
         ArrayList<String> results2 = new ArrayList<>();
 
@@ -47,7 +48,9 @@ public class AutomatonBuilder {
         for (int i = 0; i < results.size(); i++) {
             debug2(results.get(i));
         }
+
         System.out.println("Number of words in the output language: " + results.size());
+        return results;
     }
 
     public static ArrayList<String> doDFSnextStep(State state) {
@@ -310,6 +313,55 @@ public class AutomatonBuilder {
         commonPrefix = findCommonPrefix();
         currentSuffix = word.substring(commonPrefix.length());
         addSuffix(currentSuffix);
+    }
+
+    public String getCorrection(String word) {
+        ArrayList<String> results = new ArrayList<>();
+        results = doDFS();
+        String newCorrection = "";
+        int bestDistance = getLevenshteinDistance(word, results.get(0));;
+        int size = results.size();
+        for (int i = 1; i < size; i++) {
+            int levenshteinDistance = getLevenshteinDistance(word, results.get(i));
+            if (levenshteinDistance < bestDistance) {
+                bestDistance = levenshteinDistance;
+                newCorrection = results.get(i);
+            }
+        }
+        System.out.println("levenstein distance to return with " + newCorrection + " " + bestDistance);
+        return newCorrection;
+    }
+
+    /* derived from https://www.baeldung.com/java-levenshtein-distance */
+    public int getLevenshteinDistance(String wrongWord, String actualWord) {
+        int[][] dp = new int[wrongWord.length() + 1][actualWord.length() + 1];
+
+        for (int i = 0; i <= wrongWord.length(); i++) {
+            for (int j = 0; j <= actualWord.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                }
+                else if (j == 0) {
+                    dp[i][j] = i;
+                }
+                else {
+                    dp[i][j] = min(dp[i - 1][j - 1]  + costOfSubstitution(wrongWord.charAt(i - 1), actualWord.charAt(j - 1)),
+                            dp[i - 1][j] + 1,
+                            dp[i][j - 1] + 1);
+                }
+            }
+        }
+
+        return dp[wrongWord.length()][actualWord.length()];
+    }
+
+    public static int costOfSubstitution(char a, char b) {
+        return a == b ? 0 : 1;
+    }
+
+    public static int min(int... numbers) {
+        return Arrays.stream(numbers)
+                .min().orElse(Integer.MAX_VALUE);
     }
 
 }
--- Trie/src/SpellChecker.java
+++ Trie/src/SpellChecker.java
@@ -10,35 +10,40 @@ public class SpellChecker {
 
         AutomatonBuilder automaton = new AutomatonBuilder();
         automaton.create(args[0]);
-        System.out.println("membership a: " + automaton.membership("a"));
-        System.out.println("membership assent: " + automaton.membership("assent"));
-        System.out.println("membership ons: " + automaton.membership("ons"));
-        System.out.println("membership onss: " + automaton.membership("onss"));
-        System.out.println("membership e: " + automaton.membership("e"));
-        automaton.addNewWord("onss");
-        System.out.println("membership a: " + automaton.membership("a"));
-        System.out.println("membership assent: " + automaton.membership("assent"));
-        System.out.println("membership ons: " + automaton.membership("ons"));
-        System.out.println("membership onss: " + automaton.membership("onss"));
-        System.out.println("membership e: " + automaton.membership("e"));
+//        System.out.println("membership a: " + automaton.membership("a"));
+//        System.out.println("membership assent: " + automaton.membership("assent"));
+//        System.out.println("membership ons: " + automaton.membership("ons"));
+//        System.out.println("membership onss: " + automaton.membership("onss"));
+//        System.out.println("membership e: " + automaton.membership("e"));
+//        automaton.addNewWord("onss");
+//        System.out.println("membership a: " + automaton.membership("a"));
+//        System.out.println("membership assent: " + automaton.membership("assent"));
+//        System.out.println("membership ons: " + automaton.membership("ons"));
+//        System.out.println("membership onss: " + automaton.membership("onss"));
+//        System.out.println("membership e: " + automaton.membership("e"));
 
 
-//        File f1=new File(args[1]); //Creation of File Descriptor for input file
-//        String[] words=null;  //Intialize the word Array
-//        FileReader fr = new FileReader(f1);  //Creation of File Reader object
-//        BufferedReader br = new BufferedReader(fr); //Creation of BufferedReader object
-//        String s;
-//        String input="Java";   // Input word to be searched
-//        int count=0;   //Intialize the word to zero
-//        while((s=br.readLine())!=null)   //Reading Content from the file
-//        {
-//            words=s.split(" ");  //Split the word using space
-//            for (String word : words)
-//            {
-//                System.out.println(word);
-//            }
-//        }
-//
-//        fr.close();
+        File f1=new File(args[1]); //Creation of File Descriptor for input file
+        String[] words=null;  //Intialize the word Array
+        FileReader fr = new FileReader(f1);  //Creation of File Reader object
+        BufferedReader br = new BufferedReader(fr); //Creation of BufferedReader object
+        String s;
+        String input="Java";   // Input word to be searched
+        int count=0;   //Intialize the word to zero
+        while((s=br.readLine())!=null)   //Reading Content from the file
+        {
+            words=s.split(" ");  //Split the word using space
+            for (String word : words)
+            {
+                if (automaton.membership(word)) {
+                    System.out.println("Contains " + word);
+                } else {
+                    String correction = automaton.getCorrection(word);
+                    System.out.println("Does not contain " + word + " correction is " + correction);
+                }
+            }
+        }
+
+        fr.close();
     }
 }
