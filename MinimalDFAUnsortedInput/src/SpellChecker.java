f00192dd654db2080b3ad47a5e4e03979b8603f1
Nicole <19478275@sun.ac.za>
2019-04-15 23:37:41 +0200

    going to sleep

--- MinimalDFAUnsortedInput/src/SpellChecker.java
+++ MinimalDFAUnsortedInput/src/SpellChecker.java
@@ -23,27 +23,27 @@ public class SpellChecker {
 //        System.out.println("membership e: " + automaton.membership("e"));
 
 
-//        File f1=new File(args[1]); //Creation of File Descriptor for input file
-//        String[] words=null;  //Intialize the word Array
-//        FileReader fr = new FileReader(f1);  //Creation of File Reader object
-//        BufferedReader br = new BufferedReader(fr); //Creation of BufferedReader object
-//        String s;
-//        String input="Java";   // Input word to be searched
-//        int count=0;   //Intialize the word to zero
-//        while((s=br.readLine())!=null)   //Reading Content from the file
-//        {
-//            words=s.split(" ");  //Split the word using space
-//            for (String word : words)
-//            {
-//                if (automaton.membership(word)) {
-//                    System.out.println("Contains " + word);
-//                } else {
-//                    String correction = automaton.getCorrection(word);
-//                    System.out.println("Does not contain " + word + " correction is " + correction);
-//                }
-//            }
-//        }
-//
-//        fr.close();
+        File f1=new File(args[1]); //Creation of File Descriptor for input file
+        String[] words=null;  //Intialize the word Array
+        FileReader fr = new FileReader(f1);  //Creation of File Reader object
+        BufferedReader br = new BufferedReader(fr); //Creation of BufferedReader object
+        String s;
+        String input="Java";   // Input word to be searched
+        int count=0;   //Intialize the word to zero
+        while((s=br.readLine())!=null)   //Reading Content from the file
+        {
+            words=s.split(" ");  //Split the word using space
+            for (String word : words)
+            {
+                if (automaton.membership(word)) {
+                    System.out.println("Contains " + word);
+                } else {
+                    String correction = automaton.getCorrection(word);
+                    System.out.println("Does not contain " + word + " correction is " + correction);
+                }
+            }
+        }
+
+        fr.close();
     }
 }
